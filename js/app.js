ed8d013b21ff62bee4e8e0d43a2477d3432725a4
Hayden Cleaver <rexcleaver90@gmail.com>
2022-06-14 22:10:26 -0400

    implementing css and html structure

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const saleTime = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
+const saleTime = ['6:00am ', '7:00am ', '8:00am ', '9:00am ', '10:00am ', '11:00am ', '12:00pm ', '1:00pm ', '2:00pm ', '3:00pm ', '4:00pm ', '5:00pm ', '6:00pm ', '7:00pm '];
 const storeHours = 14;
 const listedStores = [];
 
-function StoreCreator (location, minCust, maxCust, avgSale){ //creates store objects 
+function StoreCreator(location, minCust, maxCust, avgSale) { //creates store objects 
 
   this.location = location;
   this.minCust = minCust;
@@ -13,12 +13,12 @@ function StoreCreator (location, minCust, maxCust, avgSale){ //creates store obj
   this.storeSales = [];
   this.total = [];
 
-  this.randomCustomers = function(){ //returns a random # of customers based upon min/max properties
+  this.randomCustomers = function () { //returns a random # of customers based upon min/max properties
     let customer = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
     return customer;
   };
 
-  this.sales = function(){ //multiplies # of customers * average sale amount and appends them to an array along with total sales made
+  this.sales = function () { //multiplies # of customers * average sale amount and appends them to an array along with total sales made
     let hourlySales = [];
     let totalSales = 0;
 
@@ -49,7 +49,7 @@ paris.sales();
 let lima = new StoreCreator('Lima', 2, 16, 4.6);
 lima.sales();
 
-function render(store){
+function render(store) { //creates hourly sale entries in html table for any store entered as argument
   let tableEl = document.getElementById('salesTable');
   let tableRowEl = document.createElement('tr');
   let locationEl = document.createElement('td');
@@ -58,7 +58,7 @@ function render(store){
   tableRowEl.appendChild(locationEl);
   locationEl.textContent = store.location;
 
-  for (let i = 0; i < store.storeSales.length; i++){
+  for (let i = 0; i < store.storeSales.length; i++) {
     let saleEl = document.createElement('td');
     tableRowEl.appendChild(saleEl);
     saleEl.textContent = store.storeSales[i];
@@ -69,7 +69,7 @@ function render(store){
   finalEl.textContent = store.total;
 }
 
-function header(){
+function header() {
   let tableEl = document.getElementById('salesTable');
   let tableRowEl = document.createElement('tr');
   let blankEl = document.createElement('th');
@@ -77,7 +77,7 @@ function header(){
   tableEl.appendChild(tableRowEl);
   tableRowEl.appendChild(blankEl);
 
-  for (let i = 0; i < storeHours; i++){
+  for (let i = 0; i < storeHours; i++) {
     let hourEl = document.createElement('th');
     tableRowEl.appendChild(hourEl);
     hourEl.textContent = saleTime[i];
@@ -88,7 +88,7 @@ function header(){
   finalEl.textContent = 'Daily Location Total';
 }
 
-function footer(){
+function footer() {
   let hourlyTotals = 0;
   let finalTotals = 0;
 
@@ -100,8 +100,8 @@ function footer(){
   tableRowEl.appendChild(firstEl);
   firstEl.textContent = 'Totals';
 
-  for (let n = 0; n < storeHours; n++){
-    for (let i = 0; i < listedStores.length; i++){
+  for (let n = 0; n < storeHours; n++) {
+    for (let i = 0; i < listedStores.length; i++) {
       hourlyTotals += parseInt(listedStores[i].storeSales[n]);
     }
 
@@ -111,7 +111,7 @@ function footer(){
     hourlyTotals = 0;
   }
 
-  for (let i = 0; i < listedStores.length; i++){
+  for (let i = 0; i < listedStores.length; i++) {
     finalTotals += parseInt(listedStores[i].total);
   }
 
